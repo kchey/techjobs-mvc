c5016400910db1c87210d8a65a8f7445f793e5ea
Kimball Chey <kimball.chey@icloud.com>
2019-06-30 19:53:08 -0500

    okay

--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,12 +4,13 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.validation.constraints.Max;
 import java.util.ArrayList;
 import java.util.HashMap;
-
-import static org.launchcode.controllers.ListController.columnChoices;
+import java.util.List;
 
 /**
  * Created by LaunchCode
@@ -20,28 +21,27 @@ public class SearchController {
 
     @RequestMapping(value = "")
     public String search(Model model) {
-        model.addAttribute("columns", columnChoices);
+        model.addAttribute("columns", ListController.columnChoices);
         return "search";
     }
 
     // TODO #1 - Create handler to process search request and display results
-    @RequestMapping(value = "results")
-    public String search(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
-        ArrayList<HashMap<String, String>> jobs;
-        if (searchType.equals("all")) {
+    @RequestMapping(value="results", method = RequestMethod.GET)
+    public String search(Model model, @RequestParam String searchTerm, @RequestParam String searchType){
+        model.addAttribute("columns", ListController.columnChoices);
+
+
+        ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+        if(searchType.equals("all")){
             jobs = JobData.findByValue(searchTerm);
-        }
-        else {
+        }else {
             jobs = JobData.findByColumnAndValue(searchType, searchTerm);
         }
-        model.addAttribute("columns", ListController.columnChoices);
+
         model.addAttribute("jobs", jobs);
-        model.addAttribute("searchType", searchType);
+
         return "search";
     }
 
 }
-
-
-
-//search "term" and "type"
